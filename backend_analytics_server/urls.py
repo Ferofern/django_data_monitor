fe1740570877610899862964a532784b4ca18eff
Felix Romero <ferofern@espol.edu.ec>
2025-08-13 15:38:19 +0000

    1

--- backend_analytics_server/urls.py
+++ backend_analytics_server/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from dashboard.views import home  # Importa la vista correcta
+from dashboard.views import index  # Importa la vista correcta
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='dashboard'),  # URL principal
+    path('', index, name='dashboard'),  # URL principal
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render  # Esto es necesario
+from django.http import HttpResponse
+import requests
+from django.conf import settings
 
-def home(request):
-    return render(request, 'dashboard/index.html')
+import requests
+from django.conf import settings
+
 def index(request):
+
+    response = requests.get(settings.API_URL)  # URL de la API
+    posts = response.json()  # Convertir la respuesta a JSON
+
+    # Número total de respuestas
+    total_responses = len(posts)
 
     data = {
         'title': "Landing Page' Dashboard",
+        'total_responses': total_responses,
     }
 
     return render(request, 'dashboard/index.html', data)
